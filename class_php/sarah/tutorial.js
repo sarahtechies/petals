71b8357dd55790c113302c81cad8be597eb3e845
sarahtechies <imoleayoakintade@gmail.com>
2023-10-07 18:55:54 +0100

    tutorials

--- class_php/sarah/tutorial.js
+++ class_php/sarah/tutorial.js
@@ -14,17 +14,57 @@
 
 // strings
 
-console . log('hello world');
+// console . log('hello world');
 
-let email = "sarah@gmail.com";
-console .log(email);
+// let email = "sarah@gmail.com";
+// console .log(email);
 
 // strings contenation
 
-let surName = 'Akintade';
-let firstName = 'Sarah';
+// let surName = 'Akintade';
+// let firstName = 'Sarah';
 
-let fullName = surName +  ' ' +  firstName;
-console . log(fullName);
+// let fullName = surName +  ' ' +  firstName;
+// console . log(fullName);
 
 // getting character
+
+// console . log(fullName[2]);
+
+// string length
+
+// console . log(fullName . length);
+
+// string method
+
+// console. log(fullName . toUpperCase());
+// let result = fullName . toLowerCase();
+// console . log(result);
+
+// let index = email. indexOf('@');
+// console. log(index);
+
+// let email = 'imole@gmail.com';
+// let result = email . lastIndexOf('i');
+
+// let result = email . slice(0, 5);
+
+// let result = email . substring(2, 7);
+
+// let result = email . replace('o', 'w');
+
+// console .log(result)
+
+let radius = 10;
+const pi = 3.14;
+
+// console . log(radius, pi);
+
+// math operations +, -,*, /,  **, %
+
+// console .log(15/5);
+// let result = radius % 3;
+
+let result =  pi * radius ** 2;
+
+console . log(result);
