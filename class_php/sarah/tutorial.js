d3d81f594c9fcc6cf608ff5e9666d8d93e0c9e12
sarahtechies <imoleayoakintade@gmail.com>
2023-10-07 17:52:28 +0100

    js

---
+++ class_php/sarah/tutorial.js
@@ -0,0 +1,13 @@
+let age = 28;
+let year = 2023;
+
+console . log(age, year);
+
+age = 30;
+console . log(age);
+
+const points = 150;
+console .log(points);
+
+var score = 30;
+console . log(score);
