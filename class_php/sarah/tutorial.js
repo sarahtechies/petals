a0d4afa0378af22c5664146337d4fb83be9f94b4
sarahtechies <imoleayoakintade@gmail.com>
2023-10-07 18:12:30 +0100

    lessons

--- class_php/sarah/tutorial.js
+++ class_php/sarah/tutorial.js
@@ -1,13 +1,30 @@
-let age = 28;
-let year = 2023;
+// let age = 28;
+// let year = 2023;
 
-console . log(age, year);
+// console . log(age, year);
 
-age = 30;
-console . log(age);
+// age = 30;
+// console . log(age);
 
-const points = 150;
-console .log(points);
+// const points = 150;
+// console .log(points);
 
-var score = 30;
-console . log(score);
+// var score = 30;
+// console . log(score);
+
+// strings
+
+console . log('hello world');
+
+let email = "sarah@gmail.com";
+console .log(email);
+
+// strings contenation
+
+let surName = 'Akintade';
+let firstName = 'Sarah';
+
+let fullName = surName +  ' ' +  firstName;
+console . log(fullName);
+
+// getting character
