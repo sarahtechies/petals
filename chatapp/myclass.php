66a91e51c0a630abceb6d588a083603cea77a5dd
sarahtechies <imoleayoakintade@gmail.com>
2023-10-10 11:29:04 +0100

    login

--- chatapp/myclass.php
+++ chatapp/myclass.php
@@ -1,46 +1,51 @@
 <?php
-
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "schoolportal";
-
-
 $db = new mysqli('localhost', 'root', '', 'chatapp');
 
 
-
-if (!$db)  {
-
-    die(mysqli_error($db));
-}
-
 class Profile{
-    function __construct() {
-       
-    }
+ function __construct(){
+  
+
+ }
+
+ function SignUp($name, $email, $phone, $password){
+  global $db;
+  $pass = password_hash($password, PASSWORD_BCRYPT);
+  $sql = $db->query("SELECT * FROM user WHERE email = '$email' AND phone ='$phone'  ");
+  if(mysqli_num_rows($sql) > 0){
+   echo "email or phone already exist";
+  }else{
+   $sql = $db->query("INSERT INTO user (name, email, phone, password) VALUES('$name', '$email', '$phone', '$pass')");
+   if ($sql) {
+    echo "operation successful";
+   }
+  };
+  
+ }
 
 
-public function signUp($name, $email, $phonenumber, $password){
+ function LogIn($email, $password){
     global $db;
-    
-    $pass = password_hash($password, PASSWORD_BCRYPT);
+    $sql = $db->query("SELECT * user WHERE email = '$email'");
+    if(mysqli_num_rows($sql) != 1)
+     echo "invalid login";
+    return;
 
-    $sql = $db->query("SELECT * FROM user WHERE email = '$email' OR phone = '$phone'");
-    if(mysqli_num_rows($sql) > 0) {
-    echo "email or phone already exists";
- }else{
-    $sql = $db->query("INSERT INTO user(name, email, phone, password) VALUES ('$name', '$email', '$phonenumber', '$pass')");
-    if($sql){echo "operation Succesful";}
-    
+    $rows = mysqli_fetch_assoc($sql);
+    if(password_verify($password, $rows['pass'])){
+        header('location:chat.html');
+        echo 'login success';
+        return;
+    }
+    echo 'invalid login';
+    return;
+ }
+};
 
+$pro = new Profile();
+if(isset($_GET['type'])){extract($_GET);
+ if($type=='signup'){$pro->SignUp($name, $email, $phone, $password);
  }
 }
 
-$pro = new Profile();
 
-if(isset($_GET['type'])){extract($_GET);
-   if($type == 'signup'){$pro->signUp($name, $email, $phonenumber, $password); }
-}
- //$pro->signUp('akintade', 'sarah@gmail.com', '08020272790', '123456');
-?>
